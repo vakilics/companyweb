0ae28a7c5c473d1d3e9ad9f754904cd6d6fdc406
Abdul Rahman Vakili <vakili@univention.de>
2024-01-05 17:16:49 +0100

    feat(companyweb): update contact form and mailing

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,11 +1,22 @@
 import streamlit as st
+from send_email import send_mail
 
 st.header("Contact Us:")
 st.write("Will be sent to: hamidgml@gmail.com")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your message ... ")
+    raw_message = st.text_area("Your message ... ")
+    #message = message + "\n" + user_email
+    #Or can format it like:
+    message = f"""\
+Subjec: New email from {user_email}
+From: {user_email}
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
     if button:
-        print("Submit is pressed!")
+        #print("Submit is pressed!")
+        send_mail(message)
+        st.info("Your email was sent successfully!")
+
--- send_email.py
+++ send_email.py
@@ -1,20 +1,17 @@
 #Note: login to google account -> manage account -> security -> under 2-factor... -> create app password
-import smtplib
-import ssl
+import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "vakilitestmail@gmail.com"
-password = "nrxbxbgeinzybxws"
+def send_mail(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "hamidgml@gmail.com"
-context = ssl.create_default_context()
-message = """
-Hi!
-This is the first test message from Vakili personal web app!
-"""
+    username = "vakilitestmail@gmail.com"
+    password = "nrxbxbgeinzybxws"
 
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    receiver = "hamidgml@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
