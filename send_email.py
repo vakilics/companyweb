ef883daa940ab7d1bffb84abda6a795013200924
Abdul Rahman Vakili <vakili@univention.de>
2024-01-10 16:53:13 +0100

    feat(mail) hide pass env

--- send_email.py
+++ send_email.py
@@ -1,13 +1,15 @@
 #Note: login to google account -> manage account -> security -> under 2-factor... -> create app password
 import smtplib, ssl
-
+import os
 
 def send_mail(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "vakilitestmail@gmail.com"
-    password = ""
+    #password = "dfr jrxeomykkolvyzoa sdf"  #NOTE: not secure! so store it in environment variable!
+    password = os.getenv("PASSW")
+
 
     receiver = "hamidgml@gmail.com"
     context = ssl.create_default_context()
